fcd9a6a9322ed075c0806c6d2aabbc119f2b8b81
kiryl1 <64288350+kiryl1@users.noreply.github.com>
2022-06-29 14:59:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
-const https = require('https');
-const url = require('url');
-const { PutObjectCommand, S3Client, ListObjectsCommand } = require('@aws-sdk/client-s3')
-const { Octokit, App } = require("octokit");
-
-var bucketName = core.getInput("bucketName")
-let client = new S3Client();
-let octokit = new Octokit({ auth: core.getInput("token") });
-var repo_list_string = core.getInput("repo")
-var repo_list = repo_list_string.split(",");
-
-
-
-async function updateDep(FILE_NAME, tag_name,repo) {
+async function updateDep(FILE_NAME, tag_name,owner) {
   console.log(FILE_NAME)
-  var TAR_URL = 'https://api.github.com/repos/kiryltestorg/' + repo + '/tarball/' + tag_name;
+  var TAR_URL = 'https://api.github.com/repos/' +owner+ '/' + repo + '/tarball/' + tag_name;
 
   var path = "Dependencies/" + repo + "/" + FILE_NAME
-  console.log(path)
+
 
   var options = {
     host: 'api.github.com',
@@ -49,10 +33,10 @@ async function updateDep(FILE_NAME, tag_name,repo) {
   });
 
 }
-async function list(repo) {
+async function list(path) {
   var params = {
     Bucket: bucketName,
-    Prefix: "Dependencies/" + repo + "/",
+    Prefix: path + "/",
   };
   
   const data = await client.send(new ListObjectsCommand(params));
@@ -62,9 +46,9 @@ async function list(repo) {
   var files = data.Contents?.filter((file) => { return file.Key.indexOf('.gz') > 0 }).sort((file1, file2) => -1 * file1.Key.localeCompare(file2.Key))
   return files
 }
-async function getLatest(repo) {
+async function getLatest(repo,owner) {
   var latest = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
-    owner: 'kiryltestorg',
+    owner: owner,
     repo: repo
   }
 
@@ -91,14 +75,32 @@ function compareVersions(v1, v2) {
   }
 }
 
+function getConfig(repo){
+  var depPath = core.getInput("depPath")
+  var fs = require('fs');
+  var config = JSON.parse(fs.readFileSync(depPath, 'utf8'));
+  return config[repo];
+
+}
+function parseConfig(cfg){
+  var path = cfg["path"]
+  var url = cfg["github_url"]
+  var org = url.split("/")[0]
+  return [path,org]
+}
+
 async function syncDependencies(repo) {
-  
-  var s3_dep_list = await list(repo)
-  var gh_latest_release = await getLatest(repo)
+  var cfg = getConfig(repo)
+  var path_and_org = parseConfig(cfg)
+  var owner = path_and_org[1]
+  var path = path_and_org[0]
+
+  var s3_dep_list = await list(repo,path)
+  var gh_latest_release = await getLatest(repo,owner)
 
   var g_tag = gh_latest_release.data.tag_name.replace("v", "")
   if (!s3_dep_list) {
-    updateDep(repo + "-" + g_tag + ".tar.gz", g_tag, repo)
+    updateDep(repo + "-" + g_tag + ".tar.gz", g_tag,owner)
     return
   }
 
@@ -109,7 +111,7 @@ async function syncDependencies(repo) {
 
 
   if (compareVersions(g_tag, s3_latest_tag)) {
-    updateDep(repo + "-" + g_tag + ".tar.gz", g_tag,repo)
+    updateDep(repo + "-" + g_tag + ".tar.gz", g_tag,owner)
   }
 
 }
