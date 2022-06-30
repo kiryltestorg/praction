f9befd83580e7a072df87445e43ceb715e68f515
kiryl1 <64288350+kiryl1@users.noreply.github.com>
2022-06-30 14:31:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ var repo_list_string = core.getInput("repo")
 var repo_list = repo_list_string.split(",");
 
 
-async function updateDep(FILE_NAME, tag_name,repo, owner) {
+async function updateDep(FILE_NAME, tag_name, repo, owner) {
   console.log(FILE_NAME)
   var TAR_URL = 'https://api.github.com/repos/' + owner + '/' + repo + '/tarball/' + tag_name;
-
+  // download location of the tarfile of a repo for a specific release 
   var path = "Dependencies/" + repo + "/" + FILE_NAME
-
+  // path where to store tar file on s3 bucket 
 
   var options = {
     host: 'api.github.com',
@@ -31,19 +31,21 @@ async function updateDep(FILE_NAME, tag_name,repo, owner) {
   https.get(options, (res) => {
 
     const writeStream = fs.createWriteStream(FILE_NAME);
-
+  
     res.pipe(writeStream);
-
+    // writing downloaded tar file to write stream 
     writeStream.on("finish", function () {
       writeStream.close();
       console.log("The download is Completed");
       var fileStream = fs.createReadStream(FILE_NAME);
+      // getting downloaded tarfile to send to s3 bucket 
       var putParams = {
         Bucket: bucketName,
         Key: path,
         Body: fileStream
       };
       const data = client.send(new PutObjectCommand(putParams));
+      //upload to s3 bucket 
 
     });
   });
@@ -59,7 +61,9 @@ async function list(path) {
   if (data.length < 0) {
     return data
   }
+  //gets all objects in the bucket folder specified by path 
   var files = data.Contents?.filter((file) => { return file.Key.indexOf('.gz') > 0 }).sort((file1, file2) => -1 * file1.Key.localeCompare(file2.Key))
+  //gets all the file names that end with the file extension .gz and sorts them desc alphabetically
   return files
 }
 async function getLatest(repo, owner) {
@@ -67,6 +71,7 @@ async function getLatest(repo, owner) {
     owner: owner,
     repo: repo
   }
+
 
   )
   return latest
@@ -95,6 +100,7 @@ function getConfig(repo) {
   var depPath = core.getInput("depPath")
   var fs = require('fs');
   var config = JSON.parse(fs.readFileSync(depPath, 'utf8'));
+  // opening dependency json file 
   return config[repo];
 
 }
@@ -107,26 +113,35 @@ function parseConfig(cfg) {
 
 async function syncDependencies(repo) {
   var cfg = getConfig(repo)
+  //read info about repo to update from config file 
   var path_and_org = parseConfig(cfg)
   var owner = path_and_org[1]
   var path = path_and_org[0]
 
   var s3_dep_list = await list(path)
+  //get latest versions of tar file on s3 bucket 
   var gh_latest_release = await getLatest(repo, owner)
+  //gets latest version of the repo on Github 
 
   var g_tag = gh_latest_release.data.tag_name.replace("v", "")
+  //remove the v and leave just the version number 
+
   if (!s3_dep_list) {
+    //if there are no versions stored on the s3 bucket of this repo 
     updateDep(repo + "-" + g_tag + ".tar.gz", g_tag, repo, owner)
     return
   }
 
   var s3_latest = s3_dep_list[0]
+  //s3_latest is sorted descending alphabetically so the first element will give the latest version in s3 bucket 
   var s3_latest_tag = s3_latest.Key.substring(s3_latest.Key.indexOf('-') + 1, s3_latest.Key.indexOf(".tar"))
+  //geting version number of latest tar file stored in s3 bucket 
   console.log(s3_latest_tag)
   console.log(g_tag)
 
 
   if (compareVersions(g_tag, s3_latest_tag)) {
+    //if version on Github is newer than one stored on s3, update depenendency 
     updateDep(repo + "-" + g_tag + ".tar.gz", g_tag, repo, owner)
   }
 
