ca7fc7babe6dcb8167a05fb7ef0bd4cd607a24b5
kiryl1 <64288350+kiryl1@users.noreply.github.com>
2022-07-06 16:05:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,24 +19,58 @@ var depPath = core.getInput("depPath")
 
 const dir = fs.opendirSync(depPath)
 let dirent
+
+function getMainRef(){
+  var ref = octokit.request('GET /repos/{owner}/{repo}/git/ref/{ref}', {
+    owner: 'kiryltestorg',
+    repo: 'mainRepo',
+    ref: 'heads/main'
+  })
+  return ref 
+}
+function createRef(hash){
+   octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+    owner: 'kiryltestorg',
+    repo: 'mainRepo',
+    ref: 'refs/heads/Pr1',
+    sha: hash
+  })
+}
+const exec = require('@actions/exec');
+
+let myOutput = '';
+let myError = '';
+
+const options = {
+listeners: {
+  stdout: (data) => {
+    myOutput += data.toString();
+  },
+  stderr: (data) => {
+    myError += data.toString();
+  }
+}
+};
+async function createPr(){
+  var ref = await getMainRef()
+  createRef(ref.data.object.sha)
+   console.log(ref.data.object.sha)
+
+   
+   await exec.exec('git', ['checkout', 'Pr1'], options);
+  
+console.log(myError)
+console.log(myOutput)
+}
+createPr()
+
+
+async function updateConfig(){
 while ((dirent = dir.readSync()) !== null) {
   console.log(dirent.name)
   var config = JSON.parse(fs.readFileSync(path.join(depPath,dirent.name)), 'utf8');
   // opening dependency json file 
   console.log(config)
-}
- octokit = new Octokit({ auth: process.env.TOKEN });
-   function getMainRef(){
-    var ref = octokit.request('GET /repos/{owner}/{repo}/git/ref/{ref}', {
-      owner: 'kiryltestorg',
-      repo: 'mainRepo',
-      ref: 'heads/main'
-    })
-    return ref 
-  }
-  async function getSha(){
-    var sha = await getMainRef()
-     console.log(sha.data.object.sha)
-  }
-  getSha()
-dir.closeSync()
+  config[SHA256]="2243"
+  await exec.exec('git', ['commit', '-m', 'updated config'], options);
+}}
